76c974d3971e42d2922dbe1fea2d12504cebf6f3
AlmatCaptain <zhagiparov_2000@mail.ru>
2020-04-26 23:48:42 +0600

    init

---
+++ app/src/main/java/com/example/imagesapi/networking/ClientApi.kt
@@ -0,0 +1,11 @@
+package com.example.imagesapi.networking
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ClientApi {
+
+    @GET("api/?key=3588873-8bb0e70fdfcef7f31eee25461&")
+    fun getAllImages(@Query("q") q: String): Call<ImageResponse>
+}
